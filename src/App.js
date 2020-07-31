daea3800e88a87c2e5cc6ae4203c3ff93beb8363
Sanya2545 <sasha2003bug@gmail.com>
2020-07-31 20:44:07 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import Header from './Header';
+import Technologies from './Technologies';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Technologies />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Technologies.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>Html</li>
+                <li>Css</li>
+                <li>Js</li>
+                <li>React</li>
+            </ul>
+        </div>
+    );
+}
+export default Technologies;
