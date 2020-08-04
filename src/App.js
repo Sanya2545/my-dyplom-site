ec70d92a222a784c7a846c0921a4b24d8922a398
Sanya2545 <sasha2003bug@gmail.com>
2020-08-04 17:44:53 +0300

    Social Web

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import Header from './Header';
-import Technologies from './Technologies';
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 const App = () => {
   return (
-    <div>
+    <div className='app-wrapper'>
+
       <Header />
-      <Technologies />
+      <Navbar />
+      <Profile/>
     </div>
   );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Profile = () =>{
+    return(
+        <div className='content'>
+        <div className='Head'>
+          <img className='landscape' src='https://thewallpaper.co//wp-content/uploads/2016/03/beautiful-beach-sunset-wide-full-hd-wallpaper-images-for-desktop-backgound-download-hd-wallpaper-amazing-high-resolution-2560x1600.jpg'></img>
+        </div>
+        <div className='Profile'>
+          <img className="avatar" src='https://lh3.googleusercontent.com/proxy/9a4AbbREVL--icbhej0VT3Css9wmqJHp-Lf4iqToWXqguAnzVpgYlS4jr43zhY8lVZFLWCTlGz3ax4S1M88ca8d3YJiieQIbOdhvxFrX6Kg6AUQrZZ8mqQRzgw'></img>
+        </div>
+        <div>
+          My posts
+          <div>
+                    New Post
+                     
+          </div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 2
+                </div>
+                <div>
+                    Post 3
+                </div>
+            </div>
+
+        </div>
+    )
+}
+export default Profile;
